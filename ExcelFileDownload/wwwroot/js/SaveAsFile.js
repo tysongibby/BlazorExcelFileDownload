51449d587db5080a3f6315e05ef40c9f8d4ea802
Tyson Gibby <tyson.gibby@outlook.com>
2022-12-07 14:13:03 -0700

    Update SaveAsFile.js
    fixed error with bytesBase64 misspelling

--- ExcelFileDownload/wwwroot/js/SaveAsFile.js
+++ ExcelFileDownload/wwwroot/js/SaveAsFile.js
@@ -1,4 +1,4 @@
-﻿function saveAsFile(fileName, buteBase64) {
+﻿function saveAsFile(fileName, bytesBase64) {
     var link = document.createElement('a');
     link.download = fileName;
     link.href = "data:application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;base64," + bytesBase64;
